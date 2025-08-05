aa7e4d031c54f29a2fb12f6706c5383684294f76
orange708 <kulkarni.dhr@northeastern.edu>
2025-08-05 18:22:13 +0800

    Bump version to 1.0.1
    Updated project version to 1.0.1 in pyproject.toml, __init__.py, and PKG-INFO to reflect new release.

--- vaultkey/__init__.py
+++ vaultkey/__init__.py
@@ -14,7 +14,7 @@ from .manager import PasswordManager
 from .generator import generate_password
 from .cli import cli
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Dhruv Kulkarni"
 __email__ = "kulkarnidhruv02@gmail.com"
 __license__ = "MIT"
